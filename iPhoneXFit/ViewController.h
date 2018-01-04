1c4225f520489452299aeff51f8757320e3f6677
bqlin <bqlins@163.com>
2018-01-04 09:48:39 +0800

    Initial Commit

---
+++ iPhoneXFit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhoneXFit
+//
+//  Created by Bq Lin on 2018/1/4.
+//  Copyright © 2018年 POLYV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iPhoneXFit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iPhoneXFit
+//
+//  Created by Bq Lin on 2018/1/4.
+//  Copyright © 2018年 POLYV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
